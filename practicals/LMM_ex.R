1d2e8068a59453a8c71a427257cc0bb59fa26523
Sara Martino <55588341+smar-git@users.noreply.github.com>
2026-02-16 23:11:13 +0100

    updated slides2 and practical 1

--- practicals/LMM_ex.R
+++ practicals/LMM_ex.R
@@ -3,33 +3,41 @@
 #| message: false
 #| code-summary: "Load libraries"
 
-library(dplyr)
 library(INLA)
-library(ggplot2)
 library(patchwork)
-library(inlabru)     
+library(inlabru)
+library(car)
+library(tidyverse)
 # load some libraries to generate nice plots
 library(scico)
 
 
 ## -----------------------------------------------------------------------------
+#| echo: true
+
+bru_options_set(control.compute = list(dic = T, waic = T))
+
+
+## -----------------------------------------------------------------------------
+#| echo: true
+#| eval: false
+
+# fit = bru(cmp, lik,
+#           options = list(control.compute = list(dic = TRUE)))
+
+
+## -----------------------------------------------------------------------------
 #| code-fold: show
-#| code-summary: "Simulate Data from a LM"
+#| code-summary: "Get body_mass data"
 
-# set seed for reproducibility
-set.seed(1234) 
+data("penguins")
+glimpse(penguins)
 
-# Fix the model parameters
-beta = c(2,0.5)
-sd_error = 0.1
 
-# simulate the data
-n = 100
-x = rnorm(n)
-y = beta[1] + beta[2] * x + rnorm(n, sd = sd_error)
 
-# create the data frame object
-df = data.frame(y = y, x = x)  
+## -----------------------------------------------------------------------------
+penguins$body_mass = penguins$body_mass/1000
+
 
 
 
@@ -42,21 +50,32 @@ df = data.frame(y = y, x = x)
 
 
 ## -----------------------------------------------------------------------------
-#| code-summary: "Fit LM in `inlabru`"
-fit.lm = bru(cmp, lik)
+#| eval: false
+# fit1 = bru(cmp, lik)
+
+
+## -----------------------------------------------------------------------------
+#| eval: true
+#| echo: true
+
+options(na.action = 'na.pass')
+fit1 = bru(cmp, lik)
+
 
 
 ## -----------------------------------------------------------------------------
 #| code-summary: "Model summaries"
 #| collapse: true
-summary(fit.lm)
+summary(fit1)
+
+
 
 
 ## -----------------------------------------------------------------------------
-new_data = data.frame(x = c(df$x, runif(10)),
-                      y = c(df$y, rep(NA,10)))
-pred = predict(fit.lm, new_data, ~ beta_0 + beta_1,
+new_data = data.frame(flipper_len  = 170:240)
+pred = predict(fit1, new_data, ~ effects,
                n.samples = 1000)
+
 
 
 ## -----------------------------------------------------------------------------
@@ -69,12 +88,47 @@ pred = predict(fit.lm, new_data, ~ beta_0 + beta_1,
 #| fig-width: 4
 #| fig-height: 4
 
-pred %>% ggplot() + 
-  geom_point(aes(x,y), alpha = 0.3) +
-  geom_line(aes(x,mean)) +
-  geom_line(aes(x, q0.025), linetype = "dashed")+
-  geom_line(aes(x, q0.975), linetype = "dashed")+
-  xlab("Covariate") + ylab("Observations")
+pred %>% ggplot() +
+  geom_line(aes(flipper_len,mean)) +
+  geom_ribbon(aes(flipper_len, ymin = q0.025, ymax = q0.975), alpha = 0.5) +
+  xlab("Flipper length") + ylab("body_mass") +
+  geom_point(data = penguins, aes(flipper_len, body_mass))
+
+
+## -----------------------------------------------------------------------------
+new_data = data.frame(flipper_len  = 170:240)
+
+pred1 = predict(fit1, new_data,
+               formula = ~ { eta = effects
+                             sigma = sqrt(1/Precision_for_the_Gaussian_observations)
+                             list(mean = eta,
+                                  q1 = qnorm(0.025, mean = eta, sd = sigma),
+                                  q2 =  qnorm(0.975, mean = eta, sd = sigma))  
+                            },
+               n.samples = 1000)
+
+
+
+## -----------------------------------------------------------------------------
+#| code-fold: true
+#| fig-cap: Data and 95% credible intervals
+#| echo: false
+#| message: false
+#| warning: false
+#| fig-align: center
+#| fig-width: 4
+#| fig-height: 4
+
+ggplot() +
+  geom_line(data = pred, aes(flipper_len,mean)) +
+  geom_ribbon(data = pred,aes(flipper_len, ymin = q0.025, ymax = q0.975), alpha = 0.5) +
+  geom_line(data = pred1$mean, aes(flipper_len,mean), color = "red") +
+  geom_line(data = pred1$q1,aes(flipper_len, mean),
+               color = "red") +
+   geom_line(data = pred1$q2,aes(flipper_len, mean),
+               color = "red") +
+  xlab("flipper length") + ylab("body_mass") +
+  geom_point(data = penguins, aes(flipper_len, body_mass))
 
 
 
@@ -83,9 +137,9 @@ pred %>% ggplot() +
 
 ## -----------------------------------------------------------------------------
 
-pred2 = predict(fit.lm, new_data, 
+pred2 = predict(fit1, new_data,
                formula = ~ {
-                 mu = beta_0 + beta_1
+                 mu = effects
                  sigma = sqrt(1/Precision_for_the_Gaussian_observations)
                  list(q1 = qnorm(0.025, mean = mu, sd = sigma),
                       q2 =  qnorm(0.975, mean = mu, sd = sigma))},
@@ -94,130 +148,93 @@ round(c(pred2$q1$mean, pred2$q2$mean),2)
 
 
 ## -----------------------------------------------------------------------------
-#| eval: false
-# ?iris
+
+penguins %>% 
+  filter(!is.na(sex)) %>%
+  ggplot() + geom_point(aes(flipper_len, body_mass, color= sex)) +
+  facet_wrap(.~sex)
 
 
 ## -----------------------------------------------------------------------------
-data("iris")
+#| eval: true
 
-iris %>% ggplot() + geom_point(aes(Sepal.Length, Petal.Length, color= Species)) +
-  facet_wrap(.~Species)
-
-
-## -----------------------------------------------------------------------------
-mod1 = lm(Petal.Length ~ Species, data  = iris)
-summary(mod1)
-
-
-
-
-## -----------------------------------------------------------------------------
-
-cmp = ~ -1 +  cov(Species, model = "iid", fixed = T, initial = log(0.001))
-lik = bru_obs(formula =Petal.Length ~ .,
-              data = iris)
-fit1b = bru(cmp, lik)
-
-
-## -----------------------------------------------------------------------------
-fit1b$summary.random$cov
-
+cmp = ~ -1 + effects( ~ sex*flipper_len, model = "fixed")
+formula = body_mass ~ .
+lik = bru_obs(formula = formula,
+              data = penguins
+              )
+fit2 = bru(cmp, lik)
 
 
 
 ## -----------------------------------------------------------------------------
-mod2 = lm(Petal.Length ~ Species:Sepal.Length, data = iris)
-mod2
+fit2$summary.random
+
+
 
 
 
 
 ## -----------------------------------------------------------------------------
 
-cmp = ~Intercept(1)  + 
-  slope(Species, Sepal.Length, model = "iid", fixed = T, initial = log(0.001))
-
-lik = bru_obs(formula =Petal.Length ~ .,
-              data = iris)
+cmp = ~ -1 + sex_intercept(sex, model = "iid", initial = log(0.001), fixed = T) +
+  sex_slope(sex, flipper_len,  model = "iid", fixed = T, initial = log(0.001))
+lik = bru_obs(formula = body_mass ~ .,
+              data = penguins %>% filter(!is.na(sex)))
 fit2b = bru(cmp, lik)
 
 
+## -----------------------------------------------------------------------------
+fit2b$summary.random$sex_intercept
+fit2b$summary.random$sex_slope
+
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+penguins %>%
+  filter(!is.na(sex)) %>%
+  ggplot() + 
+  geom_point(aes(flipper_len, body_mass, color = species)) + 
+  facet_wrap(.~sex)
+
+
+## -----------------------------------------------------------------------------
+#| echo: true
+#| eval: false
+# cmp = ~ -1 + effects( ~ sex*flipper_len, model = "fixed") +
+#   species(species, ... )
+# formula = ...
+# lik = bru_obs(formula = formula,
+#               data = penguins
+#               )
+# fit3 = bru(cmp, lik)
 
 
 
 
 ## -----------------------------------------------------------------------------
-#| code-summary: "Simulate data from a LMM"
-#|
-set.seed(12)
-beta = c(1.5,1)
-sd_error = 1
-tau_group = 1
-
-n = 100
-n.groups = 5
-x = rnorm(n)
-v = rnorm(n.groups, sd = tau_group^{-1/2})
-y = beta[1] + beta[2] * x + rnorm(n, sd = sd_error) +
-  rep(v, each = 20)
-
-df = data.frame(y = y, x = x, j = rep(1:5, each = 20))
-
-
-## -----------------------------------------------------------------------------
-#| code-fold: true
-#| fig-cap: Data for the linear mixed model example with 5 groups
-#| fig-align: center
-#| fig-width: 4
-#| fig-height: 4
-
-ggplot(df) +
-  geom_point(aes(x = x, colour = factor(j), y = y)) +
-  theme_classic() +
-  scale_colour_discrete("Group")
+fit3$summary.random$effects[,c(1,3,5)]
+fit2$summary.random$effects[,c(1,3,5)]
 
 
 
 ## -----------------------------------------------------------------------------
-# Define model components
-cmp =  ~ -1 + beta_0(1) + beta_1(x, model = "linear") +
-  u(j, model = "iid")
+#| echo: true
+#| eval: false
+# cmp = ~ -1 + effects( ~ sex*flipper_len, model = "fixed") +
+#   species1(species, ... ) +
+#   species2(species, ... )
+# formula = ...
+# lik = bru_obs(formula = formula,
+#               data = penguins
+#               )
+# fit4 = bru(cmp, lik)
+
+
 
 
 ## -----------------------------------------------------------------------------
-# Construct likelihood
-lik =  bru_obs(formula = y ~.,
-            family = "gaussian",
-            data = df)
-
-
-## -----------------------------------------------------------------------------
-#| collapse: true
-#| code-summary: "Fit a LMM in inlabru"
-fit = bru(cmp, lik)
-summary(fit)
-
-
-## -----------------------------------------------------------------------------
-#| code-fold: true
-#| code-summary: "LMM fitted values"
-#| fig-align: center
-#| fig-width: 4
-#| fig-height: 4
-
-# New data
-xpred = seq(range(x)[1], range(x)[2], length.out = 100)
-j = 1:n.groups
-pred_data = expand.grid(x = xpred, j = j)
-pred = predict(fit, pred_data, formula = ~ beta_0 + beta_1 + u)
-
-
-pred %>%
-  ggplot(aes(x=x,y=mean,color=factor(j)))+
-  geom_line()+
-  geom_ribbon(aes(x,ymin = q0.025, ymax= q0.975,fill=factor(j)), alpha = 0.5) +
-  geom_point(data=df,aes(x=x,y=y,colour=factor(j)))+
-  facet_wrap(~j)
+deltaIC(fit1, fit2, fit3, fit4, criterion = c("DIC","WAIC"))
 
 
